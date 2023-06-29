e07a1e05a8381b0ecc74375fb752ecbaea86160a
Ben-vegabond <vega.ben18@gmail.com>
2023-06-29 17:36:27 -0500

    added js to the Index.js to create interactivety

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,43 @@
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'Provide a brief description of your project:',
+        name: 'description',
+      },
+      // Add more questions as needed
+];
 
+const fs = require('fs')
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(`README file '${fileName}' created successfully!`);
+        }
+      });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const readmeContent = generateMarkdown(answers);
+        writeToFile('README.md', readmeContent);
+      });
+    
+}
 
 // Function call to initialize app
 init();
