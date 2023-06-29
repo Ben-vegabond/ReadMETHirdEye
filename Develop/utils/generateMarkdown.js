fdd9b7fdf5bba234817139bd1f32276ac1a6c6db
Ben-vegabond <vega.ben18@gmail.com>
2023-06-29 17:37:18 -0500

    added to the generateMarkdown funct

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,6 +13,34 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, feel free to reach out to me via the contact information provided below:
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
 
 `;
 }
