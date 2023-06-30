98c982f487820f457b816f6daa695ee958cf50c6
Ben-vegabond <vega.ben18@gmail.com>
2023-06-30 16:25:16 -0500

    + user questions to better fillout Readme

--- Develop/index.js
+++ Develop/index.js
@@ -12,9 +12,39 @@ const questions = [
       },
       {
         type: 'input',
+        message: 'What kind of license does your project have?',
+        name: 'license',
+      },
+      {
+        type: 'input',
         message: 'Provide a brief description of your project:',
         name: 'description',
       },
+      {
+        type: 'input',
+        message: 'How do we use your project?',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'How can users contribute to your project?',
+        name: 'contributing',
+      },
+      {
+        type: 'input',
+        message: 'Provide a brief description of your project:',
+        name: 'installation',
+      },
+      {
+        type: 'input',
+        message: 'How can a user test your project?',
+        name: 'tests',
+      },
+      {
+        type: 'input',
+        message: 'If a user has questions, how can they reach you?',
+        name: 'questions',
+      }
       // Add more questions as needed
 ];
 
